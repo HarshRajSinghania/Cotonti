e7f44a1854730effe24d788db90ab2db60d90016
Harsh Raj Singhania <raj.harshraut@gmail.com>
2026-09-18 02:37:08 +0530

    security: restrict unserialize() in EditAction comeback (CWE-502)
    Pass ['allowed_classes'=>false] to prevent PHP Object Injection
    via the cb GET/POST parameter in comment edit flow.
    See: plugins/comments/controllers/actions/EditAction.php

--- plugins/comments/controllers/actions/EditAction.php
+++ plugins/comments/controllers/actions/EditAction.php
@@ -128,7 +128,8 @@ class EditAction extends BaseAction
 
         // Comeback url params
         if (!empty($this->comeback)) {
-            $cbDecoded = unserialize(base64_decode($this->comeback));
+            // Security fix: restrict to plain arrays, prevents PHP Object Injection (CWE-502)
+        $cbDecoded = unserialize(base64_decode($this->comeback), ['allowed_classes' => false]);
             $this->cbExtensionCode = $cbDecoded[0];
             $this->cbUrlParams = $cbDecoded[1];
             $this->comeBackUrl = cot_url($this->cbExtensionCode, $this->cbUrlParams, '', true);
